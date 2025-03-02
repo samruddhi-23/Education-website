30a1760001225b529695023dec78628c4262ba37
samruddhi <samruddhisangole21.com>
2025-03-02 19:20:05 +0530

    Added all untracked files

---
+++ frontend/src/pages/alumni.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Alumni() {
+  return <h1>Alumni Page</h1>;
+}
+
+export default Alumni;
---
+++ frontend/src/pages/courses.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Courses() {
+  return <h1>Courses Offered</h1>;
+}
+
+export default Courses;
